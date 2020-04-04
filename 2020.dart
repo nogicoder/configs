53a3a537440c51aedd853fe89359cdb24fca0a9c
nogicoder <43837352+nogicoder@users.noreply.github.com>
2020-04-04 18:58:04 +0700

    Add files via upload

---
+++ 2020.dart
@@ -0,0 +1,17 @@
+main() => runApp(FirstLinesOfTheYear());
+
+class FirstLinesOfTheYear extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+            """Hope dad will improve his health, probably through training and rest well with supplement.
+                Mom will stay strong and have a good health too mentally and physically to support dad, and enjoy every moment there is with him. 
+                Sis's family and brother-in-law to always have the synergy and mutual love and respect to raise a wonderful family. 
+                Na and Ga to listen well to parents, ace in their classes and have good health. 
+                And for me, I just wish for my resilience and mental toughness to overcome all challenges ahead and protect all the love ones around me."""),
+      ),
+    );
+  }
+}
